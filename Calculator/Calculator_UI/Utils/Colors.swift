8844e9c1110e0d4533ecfec69595a50ab007888e
rithvikronaldo <86509846+rithvikronaldo@users.noreply.github.com>
2023-12-30 21:34:00 +0530

    Add files via upload

---
+++ Calculator/Calculator_UI/Utils/Colors.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+extension Color {
+    static let background: Color = Color(UIColor.systemBackground)
+    static let label: Color = Color(UIColor.label)
+    
+    
+    // MARK:- functions
+    static func getBackgroundColor(colorScheme: ColorScheme) -> Color {
+        switch colorScheme {
+        case .dark:
+            return Color(hex: "#303444")
+        case .light:
+            return Color(hex: "#EFE5CF")
+            
+        default:
+            return .white
+        }
+    }
+    
+    static func getColorForCells(colorScheme: ColorScheme) -> Color {
+        switch colorScheme {
+        case .dark:
+            return Color(hex: "#4D5672")
+        default:
+            return Color.black
+        }
+    }
+}
+
---
+++ Calculator/Calculator_UI/Utils/FontManager.swift
@@ -0,0 +1,45 @@
+import SwiftUI
+
+
+enum TypefaceOne {
+    case regular
+    case medium
+    case semibold
+    case bold
+    
+    func font(size: CGFloat) -> Font {
+        switch self {
+        case .regular:
+            return .system(size: size, weight: .regular)
+        case .medium:
+            return .system(size: size, weight: .medium)
+        case .semibold:
+            return .system(size: size, weight: .semibold)
+        case .bold:
+            return .system(size: size, weight: .bold)
+
+        }
+    }
+}
+
+
+enum TypefaceTwo {
+    case regular
+    case medium
+    case semibold
+    case bold
+    
+    func font(size: CGFloat) -> Font {
+        switch self {
+        case .regular:
+            return .custom("Ubuntu-Regular", size: size)
+        case .medium:
+            return .custom("Ubuntu-Medium", size: size)
+        case .semibold:
+            return .custom("Ubuntu-SemiBold", size: size)
+        case .bold:
+            return .custom("Ubuntu-Bold", size: size)
+            
+        }
+    }
+}
